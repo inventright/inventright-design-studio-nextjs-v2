9235ed27f4024a2ad1d19661b64f1e069511ed2d
james-inventright <249178782+james-inventright@users.noreply.github.com>
2026-01-12 01:58:50 -0500

    Add missing @tiptap/core dependency

--- components/RichTextEditor.tsx
+++ components/RichTextEditor.tsx
@@ -4,7 +4,7 @@ import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Link from "@tiptap/extension-link";
 import TextAlign from "@tiptap/extension-text-align";
-import TextStyle from "@tiptap/extension-text-style";
+import { TextStyle } from "@tiptap/extension-text-style";
 import { Color } from "@tiptap/extension-color";
 import { ResizableImage } from "./ResizableImage";
 import { Button } from "./ui/button";
